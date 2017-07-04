466a90cdde7363f8b05e813021acce8abdd06007
Alex Harley <alexbruceharley@gmail.com>
2017-07-04 12:29:06 +0200

    use isCurrentDrawer to decide who gamemaster is

--- js/components/board.js
+++ js/components/board.js
@@ -1,48 +1,73 @@
+const client = require('../services/ds')
 
 Vue.component( 'board', {
 	template: `
 <div>
   <canvas id="draw" width="500px" height="500px"></canvas>
   <div class="submissions">
-    <form action="#" >
   </div>
 </div>`,
+
+	props: ['isCurrentDrawer'],
+
 	data: function() {
 		return {
 
 		}
 	},
 	mounted: function() {
-		console.log('hell othere')
 		this.canvas = $(this.$el).find('#draw')[0];
 		this.sign = this.canvas.getContext('2d');
 		this.signArea = $('#draw');
-		this.signArea
-		.on('mousedown', this.startSignature.bind( this ) )
-		.on('mouseup', this.removeListener.bind( this ) )
-	 },
+		if (this.isCurrentDrawer) {
+			this.initialiseDrawer()
+		} else {
+			this.intialiseReceiver()
+		}
+	},
 
-	 methods: {
-		 startSignature: function(e) {     //finds where to begin the signature//
-		     this.sign.beginPath();
-		     this.sign.moveTo(e.offsetX, e.offsetY);
-		         this.moveSignature(this.sign);
-		 },
+	methods: {
+		intialiseReceiver () {
+			client.event.subscribe('line', ({ x, y }) => {
+			    this.sign.lineTo(x, y)
+			    this.sign.stroke()
+			})
 
-		 moveSignature: function(sign, e) {          //draws the signature in the canvas//
-		     this.signArea.on('mousemove', function(e) {
-		         sign.lineTo(e.offsetX, e.offsetY);
-		         sign.stroke();
-		         console.log(e.offsetX, e.offsetY);
+			client.event.subscribe('start', ({ x, y}) => {
+			    this.sign.beginPath()
+			    this.sign.moveTo(x, y)
+			})
 
-		     })
-		 },
+			client.event.subscribe('end', ({ x, y}) => {
+			    this.signArea.off('mousemove');
+			    var dataURL = canvas.toDataURL();
+			    hiddenFile.val(dataURL);
+			})
+		},
 
-		 removeListener: function(e) {     //stops drawing the signature//
-		     this.signArea.off('mousemove');
-		     var dataURL = this.canvas.toDataURL();
+	 	initialiseDrawer () {
+	 		this.signArea
+				.on('mousedown', this.startSignature.bind(this))
+				.on('mouseup', this.removeListener.bind(this))
+	 	},
 
-		 }
+		startSignature: function(e) {
+		  this.sign.beginPath()
+		  this.sign.moveTo(e.offsetX, e.offsetY)
+		  this.moveSignature(this.sign)
+		},
 
-	 }
-});
+		moveSignature: function(sign, e) {
+		  this.signArea.on('mousemove', function(e) {
+				sign.lineTo(e.offsetX, e.offsetY)
+				sign.stroke()
+				console.log(e.offsetX, e.offsetY)
+		  })
+		},
+
+		removeListener: function(e) {
+		  this.signArea.off('mousemove')
+		}
+
+	}
+})
--- js/services/ds.js
+++ js/services/ds.js
@@ -1,2 +1,3 @@
-const deepstream = require( 'deepstream.io-client-js' );
-module.exports = deepstream( 'wss://154.deepstreamhub.com?apiKey=4db68fdb-9a24-42f2-8bf6-c5a878cf43f8' );
+const deepstream = require('deepstream.io-client-js')
+module.exports =
+  deepstream('wss://154.deepstreamhub.com?apiKey=4db68fdb-9a24-42f2-8bf6-c5a878cf43f8')
