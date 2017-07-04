650787ad4dccdae106eaceea94633221f8a0ba0b
Martin Shay <martin.shay@deepstreamhub.com>
2017-07-04 16:57:18 +0200

    reduce word list

--- scripts/reset-game-state.js
+++ scripts/reset-game-state.js
@@ -3,13 +3,13 @@ const ds = require('../js/services/ds')
 
 const record = ds.record.getRecord('state').whenReady((record) => {
   const WORD_LIST = [
-    "aircraft","America","backbone","baseball","battery","bicycle","blading",
-      "bomb","bowtie","cake","circus","coal","computer","cowboy","deep","dominoes",
-      "door","electricity","frog","garbage","gingerbread-man","half","hockey","horse",
-      "iPad","key","lawnmower","light","bulb","lightsaber","mailman","mattress","music",
-      "nature","outside","palace","park","password","photograph","pinwheel","pirate",
-      "platypus","queen","roller","round","shallow","skate","ski","song","spare","spring",
-      "state","teapot","thief","toast","treasure","trip","wax","whisk","whistle"
+    "airplane","laptop","baseball","bicycle",
+      "bomb","bowtie","cake","cowboy",
+      "door","frog","hockey","horse",
+      "iPad","key","bulb","lightsaber","mailman","music",
+      "nature","palace","park","photograph","pirate",
+      "queen","round",
+      "teapot","toast"
     ];
   record.set({
     words: WORD_LIST,
